5850bdfb2445f6e67d272d5d93092d8faa79b2f9
Matthew Cory <matthew.cory@schwab.com>
2020-06-23 19:51:27 -0600

    Add broker event bus.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,25 +6,29 @@ plugins {
 subprojects {
   apply<JavaPlugin>()
   apply(plugin = "org.jetbrains.kotlin.jvm")
-  
+
   repositories {
-      // Use jcenter for resolving dependencies.
-      // You can declare any Maven/Ivy/file repository here.
-      jcenter()
-      mavenCentral()
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
   }
-  
+
   dependencies {
-      // Align versions of all Kotlin components
-      implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-  
-      // Use the Kotlin JDK 8 standard library.
-      implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  
-      // Use the Kotlin test library.
-      testImplementation("org.jetbrains.kotlin:kotlin-test")
-  
-      // Use the Kotlin JUnit integration.
-      testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Kotlin coroutine-based event bus.
+    implementation("com.github.adrielcafe.broker:broker-core:1.1.0")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
   }
 }
--- core/src/main/kotlin/kgen/core/events/EventBus.kt
+++ core/src/main/kotlin/kgen/core/events/EventBus.kt
@@ -1,9 +1,6 @@
 package kgen.core.events
 
-import java.util.concurrency.*
-import java.util.concurrency.atomic.AtomicBoolean
 
 class EventBus {
   
-  private val queues = ConcurrentHashMap<KClass<out Any>, BlockingQueue>()
 }
